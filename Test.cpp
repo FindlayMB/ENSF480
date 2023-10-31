5ca28da71ee93070895f71740150aa3c8fd1859b
FindlayMB <findlay.brown@ucalgary.ca>
2023-10-31 16:16:23 -0600

    Minor changes to Lab 5

---
+++ Test.cpp
@@ -0,0 +1,15 @@
+// 1 1 1 1 1 1 2 2 2 2 3 3 3 4 1 2 2 3 1 2
+
+// 1 : 6, 1, 1
+// 2 : 4, 2, 1
+// 3 : 3, 1
+// 4 : 1
+
+// 1 2 3 4 1 2 3 1 2
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
